fbbb5cad5d29529f3bacbc3ff9fef85b6736f418
kevinliang43 <kevinliang43@gmail.com>
2017-05-19 15:25:33 -0400

    Updated the Collection and Pred interface.

--- MazeV2/src/ICollection.java
+++ MazeV2/src/ICollection.java
@@ -5,10 +5,29 @@
 
 /**
  * Collection is a grouping of items. Used to represent either a stack or queue
+ *
  * @param <T> represents the type of item that the collection is generic over.
  */
 public interface ICollection<T> {
+
+  /**
+   * Add an item to this Collection.
+   *
+   * @param item the item to be added.
+   */
   void add(T item);
+
+  /**
+   * Removes an item from the collection.
+   *
+   * @return the item removed.
+   */
   T remove();
+
+  /**
+   * Is this Collection empty?
+   *
+   * @return boolean representing whether or not this Collection is empty.
+   */
   boolean isEmpty();
 }
